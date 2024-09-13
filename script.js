d6c23908d6a9ee8af88bca405b9ef256c1040571
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2024-09-13 16:56:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+function daysOfAYear(num){
+   let year = parseInt(num)
+	let leap = false
+	if(year % 4 === 0 && year % 100 === 0 && year % 400 === 0){
+		leap = true
+	}
+   else if(year % 4 === 0 && year % 100 !== 0){
+	    leap = true
+   }	
+   return  leap? 366 : 355
+}
+
